9e77f5209b216755789c8a97032f4e2ca495037b
Arahiza <jose.araiza@upam.edu.mx>
2018-03-19 18:56:02 -0600

    agregamos una el index en una parte con php

--- index.php
+++ index.php
@@ -45,60 +45,56 @@
 				</div>
 
 			<!-- Features -->
-				<div id="features-wrapper">
-					<section id="features" class="container">
+				<div id='features-wrapper'>
 						<header>
 							<h2>Noticias de  <strong>Hoy</strong>!</h2>
 						</header>
-						<div class="row">
-							<div class="4u 12u(mobile)">
+						
+							<center>
+							<?
+						    require_once ('conexion.php');
+						    
+						    $conn = new mysqli(servidorbd, usuariobd, psw, nombrebd);
+						    
+						    // Check connection
+						    if ($conn->connect_error) {
+						        die("Error de conexión: " . $conn->connect_error);
+						    } 
+						    
+						    $sql="SELECT * from efemerides_completas where fecha=CURDATE()";
+						    $result = $conn->query($sql);
+						    if ($result->num_rows > 0) {
+						    
+						    while($row = $result->fetch_assoc()) {
+						        
+						        
+						        $html.="<section id='features' class='container'>";
+						        $html.="<a href='#' class='image featured'><img src='images/pic01.jpg' alt='' /></a>";
+						        
+						        $html.="<p> titulo  <strong>".$row['titulo']."</strong> </p>";
+								$html.="<p> la fecha es <strong>".$row['fecha']."</strong> </p>";
+						        $html.="<p>".utf8_encode($row['contenido'])."</p>";
+						        $html.="</section>";
+						       
+						        echo $html;
+						    }
+						    } else {
+						        echo "0 results";
+						    }  
+						    
+    
 
+							?>
+						
 								<!-- Feature -->
-									<section>
-										<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-										<header>
-											<h3>Okay, so what is this?</h3>
-										</header>
-										<p>This is <strong>Strongly Typed</strong>, a free, fully responsive site template
-										by <a href="http://html5up.net">HTML5 UP</a>. Free for personal and commercial use under the
-										<a href="http://html5up.net/license">CCA 3.0 license</a>.</p>
-									</section>
+									
 
-							</div>
-							<div class="4u 12u(mobile)">
-
-								<!-- Feature -->
-									<section>
-										<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-										<header>
-											<h3>Nice! What is HTML5 UP?</h3>
-										</header>
-										<p><a href="http://html5up.net">HTML5 UP</a> is a side project of <a href="http://twitter.com/ajlkn">AJ’s</a> (= me).
-										I started it as a way to both test my <strong>skel</strong> framework and sharpen up my coding
-										and design skills a bit.</p>
-									</section>
-
-							</div>
-							<div class="4u 12u(mobile)">
-
-								<!-- Feature -->
-									<section>
-										<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-										<header>
-											<h3>Skel? What's that?</h3>
-										</header>
-										<p><strong>Skel</strong> is a lightweight framework for building responsive
-										sites and apps. All of my stuff at <a href="http://html5up.net">HTML5 UP</a> (including this
-										one) are built on this framework.</p>
-									</section>
-
-							</div>
-						</div>
-						<ul class="actions">
-							<li><a href="#" class="button icon fa-file">Tell Me More</a></li>
-						</ul>
-					</section>
+							
+							</center>
+							
 				</div>
+
+
 
 			<!-- Banner -->
 				<div id="banner-wrapper">
